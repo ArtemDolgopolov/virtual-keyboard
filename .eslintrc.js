14c0c28ed284f4218b80bfb365d0c0c03369d7a5
ArtemDolgopolov <artem.dolgopolov94@gmail.com>
2023-04-27 13:49:22 +0500

    fix: change ordinary quotes to double quotes in keyData.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,12 @@ module.exports = {
  "env": {
    "browser": true,
    "es2021": true
- },
+  },
  "extends": ["airbnb-base"],
  "parserOptions": {
    "ecmaVersion": "latest",
    "sourceType": "module"
- },
+  },
  "rules": {
    "import/extensions": ["error", "always"],
    "quotes": ["error", "double"],
