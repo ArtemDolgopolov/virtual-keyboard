ffd41bf4c19831eb93ef8244c88fb5a9a900a4f7
ArtemDolgopolov <artem.dolgopolov94@gmail.com>
2023-04-29 19:25:48 +0500

    fix: rewrite code for mouse click

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,18 @@
 import Keyboard from './keyBoard.js';
 
 const KEYBOARD = new Keyboard();
-KEYBOARD.renderKeyboard();
+let pressTrigger = null;
 
 window.addEventListener('mousedown', (e) => {
   if (e.target.tagName === 'BUTTON') {
-    e.target.classList.add('pressed-key');
+    KEYBOARD.press(e.target.dataset.code);
+    pressTrigger = e.target.dataset.code;
   }
 });
 
-window.addEventListener('mouseup', (e) => {
-  if (e.target.tagName === 'BUTTON') {
-    e.target.classList.remove('pressed-key');
+window.addEventListener('mouseup', () => {
+  if (pressTrigger) {
+    KEYBOARD.unpress(null);
+    pressTrigger = null;
   }
 });
--- src/js/keyBoard.js
+++ src/js/keyBoard.js
@@ -6,6 +6,9 @@ class Keyboard {
     this.container = null;
     this.textfield = null;
     this.keyboard = null;
+    this.pressedKeys = [];
+    this.keyNodes = [];
+    this.renderKeyboard();
   }
 
   renderKeyboard() {
@@ -22,15 +25,41 @@ class Keyboard {
     this.container.append(this.textfield, this.keyboard);
 
     this.keyData.forEach((key) => {
-      const keyNode = document.createElement('div');
+      const keyNode = document.createElement('button');
       keyNode.innerHTML = key.eng.toUpperCase();
+      keyNode.dataset.code = key.code;
       key.classes.forEach((el) => keyNode.classList.add(el));
+      this.keyNodes.push(keyNode);
       this.keyboard.append(keyNode);
     });
 
     this.textfield.addEventListener('blur', () => {
       this.textfield.focus();
     });
+  }
+
+  pressKey(code) {
+    const node = this.keyNodes.find((keyNode) => keyNode.dataset.code === code);
+    if (node) {
+      this.pressedKeys.push(code);
+      node.classList.add('pressed-key');
+    }
+  }
+
+  press(code) {
+    this.pressKey(code);
+  }
+
+  unpressKey(code) {
+    const node = this.keyNodes.find((keyNode) => keyNode.dataset.code === code);
+    if (node) {
+      this.pressedKeys = this.pressedKeys.filter((keyNode) => keyNode !== code);
+      node.classList.remove('pressed-key');
+    }
+  }
+
+  unpress(code) {
+    this.unpressKey(code);
   }
 }
 
